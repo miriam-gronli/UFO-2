56a7b7bedc192af6d8f4a6dab215eb64ea1880f0
miriam-gronli <miriam.gronli@gmail.com>
2022-11-26 22:31:54 +0100

    Initial commit

---
+++ UFO/ClientApp/src/app/app.module.ts
@@ -0,0 +1,19 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
